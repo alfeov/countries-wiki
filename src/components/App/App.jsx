a4ce47ae92511b4edc9a38a9ee1454b721a8e753
alfeov <alfeove@gmail.com>
2026-07-02 14:42:00 +0300

    Extract logic of Theme changing to ToggleThemeButton component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,18 +2,8 @@ import { RouterProvider } from 'react-router'
 import { router } from './router'
 import '@/styles/index.css'
 import '@/styles/main.scss'
-import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { selectTheme } from '@/store/theme/themeSelectors'
 
 function App() {
-  const theme = useSelector(selectTheme)
-
-  useEffect(() => {
-    const root = window.document.documentElement
-    root.classList.toggle('dark', theme)
-  }, [theme])
-
   return <RouterProvider router={router} />
 }
 
--- src/components/ui/ThemeToggleButton/ThemeToggleButton.jsx
+++ src/components/ui/ThemeToggleButton/ThemeToggleButton.jsx
@@ -3,9 +3,18 @@ import { Moon, Sun } from 'lucide-react'
 import styles from './ThemeToggleButton.module.scss'
 import { useDispatch } from 'react-redux'
 import { toggleTheme } from '@/store/theme/themeActions'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectTheme } from '@/store/theme/themeSelectors'
 
 export function ThemeToggleButton() {
   const dispatch = useDispatch()
+  const theme = useSelector(selectTheme)
+
+  useEffect(() => {
+    const root = window.document.documentElement
+    root.classList.toggle('dark', theme)
+  }, [theme])
 
   return (
     <Button variant='outline' size='icon' onClick={() => dispatch(toggleTheme)}>
